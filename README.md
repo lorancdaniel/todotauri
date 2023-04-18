aa2d380769d62defa53341cca045449ecdd4b5cd
Daniel Loranc <daniel.loranc@lodygowice.local>
2023-04-18 08:42:40 +0200

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tauri + React + Typescript + TailwindCSS
+
+easy todo app made using tauri, react, ts and tailwind
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { invoke } from "@tauri-apps/api/tauri";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container text-3xl">
+            test
+    </div>
+  );
+}
+
+export default App;
