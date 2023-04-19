82ee932f2e475bb568eb87df3fe11bc1fb4e04f7
Daniel Loranc <daniel.loranc@lodygowice.local>
2023-04-19 10:24:40 +0200

    router v2.01

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -7,8 +7,18 @@ function RootLayout() {
         <header className="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-white text-sm py-4 dark:bg-gray-800">
           <nav className="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between">
             <div className="flex flex-row items-center gap-5 mt-5 sm:justify-end sm:mt-0 sm:pl-5">
-            <NavLink to="/" className="font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500">Home</NavLink>
-            <NavLink to="add" className="font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500">Add</NavLink>
+              <NavLink
+                to="/"
+                className="font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500"
+              >
+                Home
+              </NavLink>
+              <NavLink
+                to="add"
+                className="font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500"
+              >
+                Add
+              </NavLink>
             </div>
           </nav>
         </header>
--- src/views/Add.tsx
+++ src/views/Add.tsx
@@ -1,9 +1,31 @@
+import { invoke } from "@tauri-apps/api";
+import { useState } from "react";
 function Add() {
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
+
+  async function greet() {
+    setGreetMsg(await invoke("greet", { name }));
+  }
+
   return (
     <>
-      <h1 className="text-black text-2xl">Hello, from Add</h1>
-      </>
+      <div className="flex row">
+      <form className="flex p-8"
+      onSubmit={(e) => {
+      e.preventDefault();
+      greet()
+      }}>
+      <input maxlength="30" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full mx-6 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      onChange={(e) => setName(e.currentTarget.value)}
+      placeholder="Enter a name..."
+      />
+      <button type="submit">Greet</button>
+      </form>
+      </div>
+      <p className="text-center mx-auto text-xl font-bold text-gray-900">{greetMsg}</p> 
+    </>
   );
 }
 
-export default Add; 
+export default Add;
